f2563dc5be93d98efcfbb1ca4fe2f6f9467dc968
oxalica <oxalicc@pm.me>
2023-07-10 03:13:39 +0800

    Add dev env setup

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs =
+    { self, nixpkgs, rust-overlay }:
+    {
+      devShells.x86_64-linux.default = let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          overlays = [ rust-overlay.outputs.overlays.default ];
+        };
+      in pkgs.mkShell {
+        nativeBuildInputs = [
+          (pkgs.rust-bin.fromRustupToolchain ./rust-toolchain.toml.example)
+        ];
+      };
+    };
+}
+
