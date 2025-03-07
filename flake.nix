2223d947c09e735cbed5b0a0ee7bd6a338c038c5
oxalica <oxalicc@pm.me>
2025-03-07 15:51:25 -0500

    Format and update flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,28 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
-  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
-    { self, nixpkgs, rust-overlay }:
     {
-      devShells.x86_64-linux.default = let
-        pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [ rust-overlay.outputs.overlays.default ];
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [ rust-overlay.outputs.overlays.default ];
+          };
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = [
+            (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml.example)
+          ];
         };
-      in pkgs.mkShell {
-        nativeBuildInputs = [
-          (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml.example)
-        ];
-      };
     };
 }
-
